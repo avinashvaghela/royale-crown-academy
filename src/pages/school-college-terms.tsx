5af1a9d018e636dc0853c2f29edf3e0526bf262f
Avinash Vaghela <avinashvaghela334@gmail.com>
2026-08-27 00:11:13 +0530

    Publish v8: Royale Crown Academy Ltd
    pages: Contact, FAQ, Data Protection, School/College Terms, Safeguarding Policy, SEN Support, About Us, Tutoring, Education Staffing, How It Works, For Schools & Colleges, Safeguarding, Resources, Our Team, Privacy Policy, Modern Slavery Statement, Website Disclaimer, Home, Accessibility Statement, Complaints Policy, Refund & Cancellation Policy, For Councils, Subjects, Join Our Team, Book a Consultation, For Parents, Cookie Policy, Tutoring Terms, Terms & Conditions; components: SchoolForm, StepCard, Icon, Header, Footer, CookieConsent, LegalPageLayout, TestimonialCard, SkipToContent, PageLayout, StepCard, ResourceCard, LogoWatermark, TestimonialCard, SectionReveal, SEOHead, ServiceCard, SchoolContactForm, ContactForm, TeamMemberCard, ServiceCard, FAQAccordion

--- src/pages/school-college-terms.tsx
+++ src/pages/school-college-terms.tsx
@@ -58,6 +58,6 @@ export default function SchoolCollegeTermsPage() {
       <p>
         If you have any questions about these School & College Terms, please contact us at <a href="mailto:info@royalecrownacademy.co.uk" className="font-medium text-primary underline">info@royalecrownacademy.co.uk</a>.
       </p>
-    </LegalPageLayout>
-  );
+    </LegalPageLayout>);
+
 }
