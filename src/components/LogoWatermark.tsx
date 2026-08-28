c931ad7319611a83ce1951f982579c5af63372d6
Avinash Vaghela <avinashvaghela334@gmail.com>
2026-08-28 14:39:11 +0530

    Publish v71: Royale Crown Academy
    pages: Contact, FAQ, Data Protection, School/College Terms, Safeguarding Policy, SEN Support, About Us, Tutoring, Education Staffing, How It Works, For Schools & Colleges, Safeguarding, Resources, Our Team, Privacy Policy, Home, For Councils, Subjects, Join Our Team, Book a Consultation, For Parents, Tutoring Terms, Refund & Cancellation Policy, Cookie Policy, Accessibility Statement, Website Disclaimer, Complaints Policy, Terms & Conditions, Modern Slavery Statement; components: SchoolForm, ServiceCard, LogoWatermark, StepCard, TestimonialCard, Icon, Header, Footer, CookieConsent, LegalPageLayout, TestimonialCard, SkipToContent, PageLayout, StepCard, ServiceCard, SchoolContactForm, ContactForm, FAQAccordion, ResourceCard, SectionReveal, SEOHead, TeamMemberCard

--- src/components/LogoWatermark.tsx
+++ src/components/LogoWatermark.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const logoUrl = typeof window !== 'undefined' && window.serenities
-  ? window.serenities.files.url('6bc96ae7cd439802480ecbdbdc283e0b')
-  : '';
+const logoUrl = typeof window !== 'undefined' && window.serenities ?
+window.serenities.files.url('6bc96ae7cd439802480ecbdbdc283e0b') :
+'';
 
 export default function LogoWatermark({ className = '', opacity = 0.04, size = 320 }) {
   return (
     <div
       className={`pointer-events-none absolute inset-0 -z-10 overflow-hidden ${className}`}
-      aria-hidden="true"
-    >
+      aria-hidden="true">
+
       <img
         src={logoUrl}
         alt=""
         className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 object-contain"
-        style={{ width: size, height: 'auto', opacity }}
-      />
-    </div>
-  );
+        style={{ width: size, height: 'auto', opacity }} />
+
+    </div>);
+
 }
