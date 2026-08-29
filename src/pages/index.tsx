ceafaa0e220e2ce20654cefc239e4b50e95fc7a6
Avinash Vaghela <avinashvaghela334@gmail.com>
2026-08-29 22:20:24 +0530

    Set Parents & Carers pathway card image to newly uploaded parent-and-child study photo (b2a4f10f)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   const senImageUrl = serenities.files.url('45a479344c0996e95fef0084456de99a');
   const schoolImageUrl = serenities.files.url('d8a52ffe130477f20c51a632ffc8d950');
   const professionalImageUrl = serenities.files.url('b1508abe1eb6618ff8eb1f79ee65e27f');
-  const parentImageUrl = serenities.files.url('f325f7281e4fb37f2576f08ff2a51ada');
+  const parentImageUrl = serenities.files.url('b2a4f10f6ed670fb564167add0fd0e3f');
   const studyImageUrl = serenities.files.url('a2078dfe9c07ced21d031bfe2ea9a313');
   const senLearnerImageUrl = serenities.files.url('e5fee8507681ccff4b3c2fdabe10a248');
 
